3f9b6b2b6298fd717464ec3461a26778913bd693
TJBachorz <TJBachorz@gmail.com>
2020-12-05 13:17:12 -0700

    adds cors and successfully connects users to the app

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const app = require("express")()
 const http = require("http").createServer(app)
-
-app.get("/", (request, response) => {
-    response.send("<h1>Oi oi</h1>")
+const io = require("socket.io")(http, {
+    cors: {
+        origin: "*",
+        methods: "*"
+    }
 })
+
+io.on("connection", socket => {
+    console.log("a user connected")
+})
+
+// app.get("/", (request, response) => {
+//     response.send("<h1>Oi oi</h1>")
+// })
 
 http.listen(4000, () => {
     console.log("listening")
